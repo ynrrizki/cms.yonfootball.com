9e95dd9c2885987ad84fcb84b932cf42d69a6697
ynrrizki <yanuarrizki165@gmail.com>
2026-05-01 12:02:05 +0700

    fix: cors

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,yonfootballcom-client-az3y2b-8ca7b7-72-60-74-82.traefik.me,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
